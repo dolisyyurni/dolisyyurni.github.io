458a0817b4964e2079b721611ccfe14bae7bc8c8
maxtrosy <maxtrosy@gmail.com>
2025-02-04 01:27:33 -0500

    new files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio/',  
   server: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0',
+    host: '0.0.0.0', 
   },
 })
